1f960f8f6a4ade8f21ec9ef6152598352b3c6f52
chieze-franklin <chieze.franklin@gmail.com>
2020-02-06 02:56:31 +0100

    get server up and running

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const server = require('http').createServer();
+let port = 3000;
+
+server.on('request', (req, res) => {
+    res.writeHead(200, { 'content-type': 'text/plain' });
+    res.end('Hello World!\n');
+});
+
+if (require.main == module) {
+    const portStr = process.argv[2];
+    if (portStr && !isNaN(portStr)) {
+        port = parseInt(portStr, 10);
+    }
+}
+
+server.listen(port);
