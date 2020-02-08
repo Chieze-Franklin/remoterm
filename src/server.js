e39de69639f0b51099f06cfc70160618a8b54b7b
chieze-franklin <chieze.franklin@gmail.com>
2020-02-08 21:17:09 +0100

    support clustering

--- src/server.js
+++ src/server.js
@@ -1,8 +1,5 @@
 const { spawn } = require('child_process');
-const ngrok = require('ngrok');
 const server = require('http').createServer();
-
-let port = 3000;
 
 server.on('request', (req, res) => {
     res.writeHead(200, { 'content-type': 'text/plain' });
@@ -16,21 +13,6 @@ server.on('request', (req, res) => {
     // child.on('exit', (code, signal) => res.write('\n\n'))
 });
 
-if (require.main == module) {
-    const portStr = process.argv[2];
-    if (portStr && !isNaN(portStr)) {
-        port = parseInt(portStr, 10);
-
-        (async () => {
-            // start the server
-            // server.timeout = 1000;
-            server.listen(port);
-            
-            // start ngrok
-            const url = await ngrok.connect(port);
-
-            // send this url to the server
-            console.log(url);
-        })();
-    }
-}
+// start the server
+// server.timeout = 1000;
+module.exports = server;
